e32606f67e031eedccb9d92511c8dddaa2f8402c
ishan baniya <ibaniya98@gmail.com>
2020-08-02 12:56:11 -0400

    Add logos

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 import ShoppingCartIcon from '../ShoppingCartIcon';
 import ProfileIcon from '../Profile/ProfileIcon';
 
+import Logo from './logo.png'
+
 const Navbar = () => {
     return (
         <div className="d-flex justify-content-between align-items-center">
             <div>
-                <Link to="/"> Home</Link>
+                <Link to="/">
+                    <img src={Logo} alt="Namaste Savannah Official Logo" height={50} />
+                </Link>
             </div>
 
             <div style={{ fontSize: 24 }} >
